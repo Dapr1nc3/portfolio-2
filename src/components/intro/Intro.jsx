256bee9f4999129130063788cfa146ee7d24e6a7
dapr1nc3 <ddaprincem84@gmail.com>
2022-03-25 16:35:12 -0400

    added npm ityped, finished styling and animations on intro.jsx and intro.scss

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { init } from "ityped";
+import { useRef } from "react";
 import "./Intro.scss";
 
 const Intro = () => {
-  return <div className="intro" id="intro"></div>;
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Front-End", "Back-End", "UI/UX Designer"],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="/" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Devin Minnis</h1>
+          <h3>
+            Web Developer: <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="/" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
