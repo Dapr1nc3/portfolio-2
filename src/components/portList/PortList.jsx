9ee890d9e6a17268e5199d8e2aff36470dfe8a3d
dapr1nc3 <ddaprincem84@gmail.com>
2022-03-25 17:19:22 -0400

    added portfolio animations and cards to hold projects when updated

---
+++ src/components/portList/PortList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PortList = () => {
+  return <div>PortList</div>;
+};
+
+export default PortList;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,61 @@ import React from "react";
 import "./Portfolio.scss";
 
 const Portfolio = () => {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://www.livetecs.com/wp-content/uploads/2019/05/Project-Management-2.png"
+            alt=""
+          />
+          <h3>Project 1</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://www.livetecs.com/wp-content/uploads/2019/05/Project-Management-2.png"
+            alt=""
+          />
+          <h3>Project 2</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://www.livetecs.com/wp-content/uploads/2019/05/Project-Management-2.png"
+            alt=""
+          />
+          <h3>Project 3</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://www.livetecs.com/wp-content/uploads/2019/05/Project-Management-2.png"
+            alt=""
+          />
+          <h3>Project 4</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://www.livetecs.com/wp-content/uploads/2019/05/Project-Management-2.png"
+            alt=""
+          />
+          <h3>Project 5</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://www.livetecs.com/wp-content/uploads/2019/05/Project-Management-2.png"
+            alt=""
+          />
+          <h3>Project 6</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
