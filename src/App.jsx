9158c0f3c7744b1c7690eb64e40ee03d75f612c4
dapr1nc3 <ddaprincem84@gmail.com>
2022-03-25 14:54:55 -0400

    added animated features to Topbar and a menu as a side bar with anchors

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
+import { useState } from "react";
 import "./App.scss";
 
 const App = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./Topbar.scss";
 import { Person, Mail } from "@material-ui/icons";
 
-const Topbar = () => {
+const Topbar = ({ menuOpen, setMenuOpen }) => {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
@@ -20,7 +20,7 @@ const Topbar = () => {
           </div>
         </div>
         <div className="right">
-          <div className="hamburg">
+          <div className="hamburg" onClick={() => setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
